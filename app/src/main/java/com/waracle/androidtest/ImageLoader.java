27ad9da089dd9c7d0bff23af9f47d9d6e43b5ec3
UnreachableCode <cfin@protonmail.com>
2017-08-17 09:04:20 +0100

    Loading image data moved to async task within imade loader

--- app/src/main/java/com/waracle/androidtest/ImageLoader.java
+++ app/src/main/java/com/waracle/androidtest/ImageLoader.java
@@ -2,16 +2,21 @@ package com.waracle.androidtest;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.ImageView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.InvalidParameterException;
-
 /**
  * Created by Riad on 20/05/2015.
  */
@@ -19,7 +24,9 @@ public class ImageLoader {
 
     private static final String TAG = ImageLoader.class.getSimpleName();
 
-    public ImageLoader() { /**/ }
+    private WeakReference<ImageView> imageViewReference;
+
+    public ImageLoader() { }
 
     /**
      * Simple function for loading a bitmap image from the web
@@ -32,39 +39,20 @@ public class ImageLoader {
             throw new InvalidParameterException("URL is empty!");
         }
 
+        imageViewReference = new WeakReference<ImageView>(imageView);
+
         // Can you think of a way to improve loading of bitmaps
         // that have already been loaded previously??
 
         try {
-            setImageView(imageView, convertToBitmap(loadImageData(url)));
+            loadImageData(url, imageView);
         } catch (IOException e) {
             Log.e(TAG, e.getMessage());
         }
     }
 
-    private static byte[] loadImageData(String url) throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-        InputStream inputStream = null;
-        try {
-            try {
-                // Read data from workstation
-                inputStream = connection.getInputStream();
-            } catch (IOException e) {
-                // Read the error from the workstation
-                inputStream = connection.getErrorStream();
-            }
-
-            // Can you think of a way to make the entire
-            // HTTP more efficient using HTTP headers??
-
-            return StreamUtils.readUnknownFully(inputStream);
-        } finally {
-            // Close the input stream if it exists.
-            StreamUtils.close(inputStream);
-
-            // Disconnect the connection
-            connection.disconnect();
-        }
+    private void loadImageData(String url, ImageView imageView) throws IOException {
+        new GetCakeFeedTask(imageView).execute(new URL(url));
     }
 
     private static Bitmap convertToBitmap(byte[] data) {
@@ -73,5 +61,55 @@ public class ImageLoader {
 
     private static void setImageView(ImageView imageView, Bitmap bitmap) {
         imageView.setImageBitmap(bitmap);
+    }
+
+
+    private class GetCakeFeedTask extends AsyncTask<URL, Void, byte[]> {
+
+        private final WeakReference<ImageView> imageViewRef;
+
+        public GetCakeFeedTask(ImageView imageView)
+        {
+            imageViewRef = imageViewReference;
+        }
+
+        @Override
+        protected byte[] doInBackground(URL... urls) {
+            URL url = urls[0];
+            HttpURLConnection connection = null;
+            InputStream inputStream = null;
+            try {
+                 connection = (HttpURLConnection) url.openConnection();
+
+                    // Read data from workstation
+                    inputStream = connection.getInputStream();
+
+
+                // Can you think of a way to make the entire
+                // HTTP more efficient using HTTP headers??
+
+                return StreamUtils.readUnknownFully(inputStream);
+            } catch (IOException e) {
+                // Read the error from the workstation
+                inputStream = connection.getErrorStream();
+                return null;
+            } finally {
+                // Close the input stream if it exists.
+                StreamUtils.close(inputStream);
+
+                // Disconnect the connection
+                connection.disconnect();
+            }
+        }
+
+        protected void onPostExecute(byte[] array) {
+            if (imageViewRef != null) {
+                ImageView imageView = imageViewRef.get();
+                if (imageView != null) {
+                    imageView.setImageBitmap(convertToBitmap(array));
+                }
+            }
+
+        }
     }
 }
