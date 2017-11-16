5ff86e2929c35e2a91a3c0aa44d327921b88f403
Cesar Diez Sanchez <cesaryomismo@gmail.com>
2017-11-16 10:10:46 -0500

    Fixed crash due to image sync loading for every item on the list.

--- app/src/main/java/com/waracle/androidtest/ImageLoader.java
+++ app/src/main/java/com/waracle/androidtest/ImageLoader.java
@@ -5,21 +5,26 @@ import android.graphics.BitmapFactory;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.ImageView;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.InvalidParameterException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by Riad on 20/05/2015.
  */
 public class ImageLoader {
 
+    private static final int DEFAULT_THREAD_COUNT = 3;
     private static final String TAG = ImageLoader.class.getSimpleName();
 
-    public ImageLoader() { /**/ }
+    ExecutorService executorService;
+    ImageLoader() {
+        executorService = Executors.newFixedThreadPool(DEFAULT_THREAD_COUNT);
+    }
 
     /**
      * Simple function for loading a bitmap image from the web
@@ -35,10 +40,26 @@ public class ImageLoader {
         // Can you think of a way to improve loading of bitmaps
         // that have already been loaded previously??
 
-        try {
-            setImageView(imageView, convertToBitmap(loadImageData(url)));
-        } catch (IOException e) {
-            Log.e(TAG, e.getMessage());
+        executorService.submit(new RemoteImageLoading(imageView, url));
+    }
+
+    static class RemoteImageLoading implements Runnable {
+
+        private final ImageView imageView;
+        private final String url;
+
+        public RemoteImageLoading(ImageView imageView, String url) {
+            this.imageView = imageView;
+            this.url = url;
+        }
+
+        @Override
+        public void run() {
+            try {
+                setImageView(imageView, convertToBitmap(loadImageData(url)));
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
         }
     }
 
