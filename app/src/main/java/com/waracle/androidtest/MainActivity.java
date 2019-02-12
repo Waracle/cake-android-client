78d6d3c6855e79cf247a8400f1fc36366d6b7ec3
Tom Reed <t.reed@waracle.com>
2019-02-12 16:06:01 +0000

    Version updates and temp JSON with temp fixed image paths

--- app/src/main/java/com/waracle/androidtest/MainActivity.java
+++ app/src/main/java/com/waracle/androidtest/MainActivity.java
@@ -28,8 +28,7 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static String JSON_URL = "https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/" +
-            "raw/8dd19a88f9b8d24c23d9960f3300d0c917a4f07c/cake.json";
+    private static String JSON_URL = "https://gist.githubusercontent.com/t-reed/739df99e9d96700f17604a3971e701fa/raw/1d4dd9c5a0ec758ff5ae92b7b13fe4d57d34e1dc/waracle_cake-android-client";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
